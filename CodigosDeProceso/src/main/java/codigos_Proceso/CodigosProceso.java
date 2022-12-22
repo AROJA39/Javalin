6b5f1a1646a18fdb6d182a0cd46fd176ef2f0986
adriana <apatriciarojas@ucundinamarca.edu.co>
2022-12-22 14:06:53 -0500

    Funciona erfecto

--- CodigosDeProceso/src/main/java/codigos_Proceso/CodigosProceso.java
+++ CodigosDeProceso/src/main/java/codigos_Proceso/CodigosProceso.java
@@ -9,11 +9,12 @@ import org.json.simple.parser.JSONParser;
 
 
 public class CodigosProceso {
-//ingresar en rl navegador http://localhost:8000/hello/011000
+//ingresar en rl navegador http://localhost:8000/DEPOSITOS_CUENTA/211000
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8000);
-        app.get("/hello/:name", ctx -> {
-        	String valor = ctx.pathParam("name");
+        app.get("/:proceso/:codigo", ctx -> {
+        	String codigo = ctx.pathParam("codigo");
+        	String proceso = ctx.pathParam("proceso");
         	jsonFromURL jsonFromURL = new jsonFromURL();
             String strJson = jsonFromURL.getJSONFromURL(
                 "http://localhost:8080/json/codigo_de_proceso_v1.json"
@@ -24,24 +25,23 @@ public class CodigosProceso {
                 JSONObject mainJsonObject = (JSONObject) object;
                  
                 /*************** Phone Numbers ****************/
-                JSONArray jsonArrayPhoneNumbers = (JSONArray) mainJsonObject.get("RETIROS_/_AVANCES");
+                JSONArray jsonArrayPhoneNumbers = (JSONArray) mainJsonObject.get(proceso);
                 
                 for (int i = 0; i < jsonArrayPhoneNumbers.size(); i++) {
                     JSONObject jsonPhoneNumber = (JSONObject) jsonArrayPhoneNumbers.get(i);
 
-                    String type = (String) jsonPhoneNumber.get(valor);
-                    System.out.println(valor + type); 
+                    String type = (String) jsonPhoneNumber.get(codigo);
+                    System.out.println(codigo+" " + type); 
                     if (type != null) {
-                        ctx.result("RETIROS_/_AVANCES: 011000 - " + type);
+                        ctx.result(proceso+": " +codigo+" -  "+ type);
                     } else {
-                        ctx.result("No Existe");
+                        ctx.result("No Existe el "+ proceso+" con " +codigo);
                     }
                 }
-                
-               
             }
             catch(Exception ex) {
                 ex.printStackTrace();
+                ctx.result("No Existe el "+ proceso+" con " +codigo);
             }
         });
     }
